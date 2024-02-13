f79034b9db6a954bcbd960faf04870b6e399930f
Qisti Fadilah <qistifadilah01@gmail.com>
2024-02-13 15:40:02 +0700

    Menambahkan Form Supplier

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="footer clearfix text-muted">
+        <div class="float-start">
+            <p>2024 &copy; XII RPL 2</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                by <a href="https://www.instagram.com/unitydevid/">Unity Development</a></p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/supplier/create.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.main')
+@section('title', 'Create | Supplier')
+@section('content')
+    <div class="row mb-2">
+        <div class="col-12 col-md-6 order-md-1 order-last">
+            <h3>Supplier</h3>
+        </div>
+        <div class="col-12 col-md-6 order-md-2 order-first">
+            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="#">
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item" aria-current="page">
+                        <a href="#">
+                            Supplier
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item">Create</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    {{-- card --}}
+    <div class="card">
+
+        {{-- card-header --}}
+        <div class="card-header bg-light-primary pt-3 pb-2 mb-2">
+            <h4 class="card-title">Form Supplier</h4>
+        </div>
+
+        {{-- card-body --}}
+        <form action="#" method="POST">
+            @csrf
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="nama_supplier">Nama Supplier</label>
+                            <input type="text" id="nama_supplier" class="form-control" name="nama_supplier"
+                                placeholder="Masukan nama supplier" value="">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="no_tlp">No. Telephone</label>
+                            <input type="text" id="no_tlp" class="form-control" name="no_tlp"
+                                placeholder="ex. 081234567896" value="">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <textarea id="alamat" class="form-control" name="alamat" placeholder="Masukan alamat"></textarea>
+                </div>
+            </div>
+
+            {{-- card footer --}}
+            <div class="card-footer bg-body-tertiary" style="border: none;">
+                <div class="row">
+                    <div class="col-6 d-flex justify-content-start">
+                        <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                        <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end">
+                        <input type="button" class="btn btn-primary" value="Back" onclick="window.location.href='#'">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
